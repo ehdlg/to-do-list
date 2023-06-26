0dac9ded56315d3f4849c9f8085dad128fc0e17e
Enrique Hidalgo <121901959+ehdlg@users.noreply.github.com>
2023-06-26 10:54:20 +0200

    Module with ToDo class that create a to-do objetct with title, description, dueDate and priority as their properties. Also added setters and getters functions

--- src/todo.js
+++ src/todo.js
@@ -1,10 +1,19 @@
-export default function (title, description, dueDate, priority) {
-  const getTitle = () => title;
-  return {
-    title,
-    description,
-    dueDate,
-    priority,
-    getTitle,
-  };
+export default class ToDo {
+  constructor(title, description, dueDate, priority) {
+    this.title = title;
+    this.description = description;
+    this.dueDate = dueDate;
+    this.priority = priority;
+  }
+
+  get title() {
+    return 'The title is: ' + this._title;
+  }
+
+  set title(newTitle) {
+    if (newTitle.length > 5) {
+      throw new Error('The title must be less than 5 characters long');
+    }
+    this._title = newTitle;
+  }
 }
