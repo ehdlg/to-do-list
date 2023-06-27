5653b04d7ca942d6a0e93eb224f43cefee5c6d33
Enrique Hidalgo <121901959+ehdlg@users.noreply.github.com>
2023-06-27 13:54:50 +0200

    Added AddEventListeners for the projects delete buttons. They are triggered when the user click the button and confirms their choice

--- src/index.js
+++ src/index.js
@@ -1,12 +1,23 @@
 import './styles/layout.css';
-import { addNewProject, addNewTodo } from './functions';
-import { printProjects } from './dom';
+import { addNewProject, addNewTodo, deleteProject } from './functions';
+import { printProjects, removeElement } from './dom';
+import { updateStorage } from './localstorage';
+
+printProjects();
 
 const btnSubmitProject = document.querySelector('#submit-project');
 const inputProjectName = document.querySelector('#name-project');
+const deleteProjectBtns = document.querySelectorAll('.delete-project');
 
 btnSubmitProject.addEventListener('click', function () {
   addNewProject(inputProjectName.value);
 });
 
-printProjects();
+deleteProjectBtns.forEach((deleteBtn) => {
+  deleteBtn.addEventListener('click', function () {
+    if (confirm('Are you sure you want to delete this project?')) {
+      deleteProject(this.dataset.projectname);
+      removeElement(this.parentNode);
+    }
+  });
+});
